9e2373c7381019332986e1c9f689d48441441005
Vale <valentinahdz0296@gmail.com>
2024-06-05 23:06:41 -0600

    creacion de hojas de estilos para tareas

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList} from './TodoList';
 import { TodoCreateButton } from './TodoCreateButton';
 import { TodoItem } from './TodoItem';
+
+const defaultTodos =[
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar Curso', completed: false},
+  {text: 'Llorar con la llorona', completed: true},
+  {text: 'lalala', completed: false},
+  {text: 'jasdfkajf', completed: false},
+
+  ];
+
 function App() {
   return (
-    <div className="App">
+    <> 
       
-    <TodoCounter/>
+    <TodoCounter completed={15} total={20}/>
     <TodoSearch/>
 
     <TodoList>
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
+    {defaultTodos.map(todo => (
+      <TodoItem key={todo.text} 
+      text={todo.text}
+      completed={todo.completed}/>
+    ))}      
     </TodoList>
 
 <TodoCreateButton/>
-    </div>
+</>
   );
 }
 
---
+++ src/TodoSearch.js
@@ -0,0 +1,9 @@
+import './TodoSearch.css'
+function TodoSearch(){
+    return(
+      <input placeholder="Ingresa una Tarea" 
+      className="TodoSearch"/>
+    );
+  }
+
+  export{TodoSearch};
