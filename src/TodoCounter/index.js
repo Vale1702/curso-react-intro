55bc1d1aceccf46481d4d1ae958a3e5f8790eca8
Vale <valentinahdz0296@gmail.com>
2024-08-08 11:12:25 -0600

    Se realizan cambios en hoja de estilos de TodoCounter y TodoSearch

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './TodoCounter.css';
 
-function TodoCounter({ totalTodos, completedTodos}){
+function TodoCounter({ totalTodos, completedTodos, loading}){
    
 return(
- <h1 className='TodoCounter'>
+ <h1 className={`TodoCounter ${!!loading && "TodoCounter--loading"}`}>
   Haz completados <span>{completedTodos} </span>
   de <span>{totalTodos} </span> TODOS
  </h1>
